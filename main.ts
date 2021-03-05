5e7cd32aa523e1924348473d71bb24541e2dc22a
bindmukesh <41509934+bindmukesh@users.noreply.github.com>
2021-03-05 16:14:49 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(9)
+    basic.pause(100)
+    basic.showNumber(8)
+    basic.pause(100)
+    basic.showNumber(7)
+    basic.pause(100)
+    basic.showNumber(6)
+    basic.pause(100)
+    basic.showNumber(5)
+    basic.showNumber(4)
+})
+basic.forever(function () {
+	
+})
